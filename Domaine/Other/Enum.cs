7cb86ea8fa6acf216e7a67cbbd902dcbef42c7d3
Vhalia <maxlegrelle@gmail.com>
2021-12-13 12:57:08 +0100

    add - untracked files

---
+++ Domaine/Other/Enum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_Market_Vinci.Domaine.Other
+{
+    public enum States
+    {
+        Envoye, EnAttente, EnLigne, Refuse
+    };
+
+    public enum SentTypes
+    {
+
+        ADonner, AVendre, AEchanger
+    };
+
+    public enum Types
+    {
+        Maison, Jardin, Famille, Vetements, Accessoires, Loisirs, Electroniques
+    };
+
+
+}
