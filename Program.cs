13bdf17417e31f21b843f1972fdafba0c30b51e1
Vhalia <maxlegrelle@gmail.com>
2021-12-14 22:55:26 +0100

    add - config azure vault

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
 using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Back_Market_Vinci.DataServices;
 using Back_Market_Vinci.Domaine;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
@@ -25,26 +28,21 @@ namespace Back_Market_Vinci
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-
-                })
             .ConfigureAppConfiguration((context, config) =>
             {
                 var builtConfig = config.Build();
-                var vaultUrl = builtConfig["VaultUrl"];
-                var keyVaultClient = new KeyVaultClient(async (authority, resource, scope) =>
-                {
-                    var credential = new DefaultAzureCredential(false);
-                    var token = credential.GetToken(
-                        new Azure.Core.TokenRequestContext(
-                            new[] { "https://vault.azure.net/.default" }
-                            )
-                        );
-                    return token.Token;
-                });
-                config.AddAzureKeyVault(vaultUrl, keyVaultClient, new DefaultKeyVaultSecretManager());
+                var vaultUrl = builtConfig["AzureVault:VaultUrl"];
+                var clientId = builtConfig["AzureVault:ClientId"];
+                var tenantId = builtConfig["AzureVault:TenantId"];
+                var clientSecret = builtConfig["AzureVault:ClientSecret"];
+                var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+                var client = new SecretClient(new Uri(vaultUrl), credential);
+                config.AddAzureKeyVault(client, new KeyVaultSecretManager());
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+
             });
     }
 }
